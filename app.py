dd758c60e0e3491166d9d3f2ba0f00590e92e373
ankurCES <ankur.nair@cesltd.com>
2018-12-27 12:47:02 +0530

    Edited endpoints

--- app.py
+++ app.py
@@ -6,14 +6,14 @@ from readmission_prediction import run_modelling
 
 app = Flask(__name__)
 
-@app.route('/get_prediction', methods=['POST'])
+@app.route('/predict', methods=['POST'])
 def index():
     payload = request.get_json()
     ret_val = start_prediction(payload)
     ret_val = json.dumps(ret_val)
     return ret_val
 
-@app.route('/run_modelling', methods=['GET'])
+@app.route('/modelling', methods=['GET'])
 def learn():
     ret_val = run_modelling()
     return json.dumps(ret_val)
